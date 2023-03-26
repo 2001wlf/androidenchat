2701087687796d16dbe54bd92f2a51650559e9de
王龙飞 <wanglongfei@wanglongfeideMacBook-Air.local>
2023-03-26 19:04:57 +0800

    success maps

--- bar/app/src/main/java/com/example/bar/fragment/HomeFragment.java
+++ bar/app/src/main/java/com/example/bar/fragment/HomeFragment.java
@@ -16,12 +16,12 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
-        MapView mapView = (MapView) findViewById(R.id.map);
+        MapView mapView = (MapView) rootView.findViewById(R.id.map);
         mapView.onCreate(savedInstanceState);// 此方法必须重写
         AMap aMap = mapView.getMap();
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        return rootView;
     }
 }
