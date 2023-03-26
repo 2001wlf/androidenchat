69c96726839ba4c7eee4c9e636cc0c3a784689fd
王龙飞 <wanglongfei@wanglongfeideMacBook-Air.local>
2023-03-26 19:01:43 +0800

    last modify

--- bar/app/src/main/java/com/example/bar/fragment/HomeFragment.java
+++ bar/app/src/main/java/com/example/bar/fragment/HomeFragment.java
@@ -8,12 +8,20 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.MapView;
 import com.example.bar.R;
 
 public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MapView mapView = (MapView) findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);// 此方法必须重写
+        AMap aMap = mapView.getMap();
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
